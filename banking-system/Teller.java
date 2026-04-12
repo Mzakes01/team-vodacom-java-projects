14028c590e21c52eace34203d57e998611be057b
Thabang Leemisa <senosithabang9@gmail.com>
2026-04-12 18:22:02 +0200

    Added Teller Menu and Basic Structure

--- banking-system/Teller.java
+++ banking-system/Teller.java
@@ -1,5 +1,12 @@
 public class Teller {
+    // ===== SHOW MENU =====
     public void showMenu() {
-        System.out.println("Teller Menu");
-    }
+        System.out.println("\n--- Teller Menu ---");
+        System.out.println("1. Deposit");
+        System.out.println("2. Withdraw");
+        System.out.println("6. Open Account");
+        System.out.println("7. Close Account");
+        System.out.println("8. View Any Account");
+        System.out.println("9. Set Overdraft Limit");
+        System.out.print("Select option: ");
 }
