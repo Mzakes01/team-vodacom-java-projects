b4324f5d89d0bda346eb3c31e2b4b3d54fdd2816
Thabang Leemisa <senosithabang9@gmail.com>
2026-04-15 22:28:43 +0200

    Update BankAccount.java
    You on the right . The only thing to fix is swapping String for Transaction in your list  and once you do that in the field, the getter, and the deposit method, everything should be fine

--- BankAccount.java
+++ BankAccount.java
@@ -8,7 +8,7 @@ public abstract class BankAccount {
     private String accountNumber;
     private String ownerName;
     private double balance;
-    private List<String> transactions;
+    private List<String> transactions; // We must store Transaction objects, not plain text.
 
     public BankAccount(String accountNumber, String ownerName, double initialBalance) {
         this.accountNumber = accountNumber;
@@ -29,7 +29,7 @@ public abstract class BankAccount {
         return balance;
     }
 
-    public List<String> getTransactions() {
+    public List<String> getTransactions() { //Method must return the correct type .SO instead of string it should be transaction
         return transactions;
     }
 
@@ -44,7 +44,7 @@ public abstract class BankAccount {
             throw new IllegalArgumentException("Deposit amount must be greater than 0");
         }
         balance += amount;
-        transactions.add("Deposit: R" + amount + " | Balance: R" + balance);
+        transactions.add("Deposit: R" + amount + " | Balance: R" + balance);//Must create a Transaction object instead of a String.
     }
 
     public void printStatement() {
