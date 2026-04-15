aaf56e814d5f892a6d9d8c423aeeca5851e88eaf
Tanyaradzwa Chitaka <tanyaradzwachitaka05@gmail.com>
2026-04-15 20:02:55 +0200

    implemented SavingsAccount correctly in banking.model

--- banking-system/SavingsAccount.java
+++ banking-system/SavingsAccount.java
@@ -1,8 +1,56 @@
 package banking.model;
 
-public class SavingsAccount {
-    // Will extend BankAccount once it's available
+public class SavingsAccount extends BankAccount {
+    //The minimum balance that must remain in the account at all times.
+     
+
+    private final double minimumBalance;
+
+    // Constructor
+    public SavingsAccount(String accountNumber, String ownerName, double initialBalance, double minimumBalance) {
+        super(accountNumber, ownerName, initialBalance);
+        this.minimumBalance = minimumBalance;
+    }
+
+    // Getter
+    public double getMinimumBalance() {
+        return minimumBalance;
+    }
+
+    // Override withdraw method
+    @Override
+   
+    public void withdraw(double amount) throws InsufficientFundsException {
+        
+        if (amount <= 0) {
+            throw new IllegalArgumentException("Amount must be greater than 0");
+        }
+
+        if (getBalance() - amount < minimumBalance) {
+            throw new InsufficientFundsException(amount, getBalance() - minimumBalance);
+        }
+
+        // Deduct money
+        deductFromBalance(amount);
+
+        // Record transaction
+        Transaction transaction = new Transaction(
+                Transaction.Type.WITHDRAWAL,
+                amount,
+                getBalance()
+        );
+
+        addTransaction(transaction);
+    }
+
+    // toString method
+    @Override
+    public String toString() {
+        return "SavingsAccount{" +
+                "accountNumber='" + getAccountNumber() + '\'' +
+                ", owner='" + getOwnerName() + '\'' +
+                ", balance=" + getBalance() +
+                ", minimumBalance=" + minimumBalance +
+                '}';
+    }
 }
-git add .
-git commit -m "started SavingsAccount class"
-git push
